65497e83df9e37d11ed64c09297612affbe255af
DelanHuang <77751806+DelanHuang@users.noreply.github.com>
2021-02-17 16:52:49 -0700

    added header files

---
+++ Lab4/main.c
@@ -0,0 +1,12 @@
+#include "msp.h"
+#include "core_cm4.h"
+#include "cmsis_gcc.h"
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+}
