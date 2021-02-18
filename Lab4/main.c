eb14dcd88f401f685ee2097a6b92a9b69adf71f4
DelanHuang <77751806+DelanHuang@users.noreply.github.com>
2021-02-17 17:36:04 -0700

    Added pwm files from last lab

--- Lab4/main.c
+++ Lab4/main.c
@@ -1,12 +1,41 @@
 #include "msp.h"
 #include "core_cm4.h"
 #include "cmsis_gcc.h"
+#include "pwm.h"
 
 
 /**
  * main.c
  */
+
+void PORT1_IRQHandler(void){
+    __NVIC_DisableIRQ(PORT1_IRQn); //disable since we're in the interrupt
+
+    if(P1->IFG & BIT1){
+        P1->IFG &= ~BIT1;
+    }
+    while((P1->IN & BIT1) == 0x00){ //RESETS timer while P1.1 is being held
+        stop_pwm(); //stops/reset
+        TIMER_A0->R = 0; //sets counter register to 0
+    }
+    //re-initialize timer stuff and begin
+    config_pwm_timer();
+    start_pwm();
+
+    __NVIC_EnableIRQ(PORT1_IRQn); //enable interrupt since we are about to exit handler
+}
+
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+
+    stop_pwm();
+    config_switch_interrupt();
+    config_pwm_timer();
+    config_pwm_gpio();
+    start_pwm();
 }
+
+
+
+
---
+++ Lab4/pwm.h
@@ -0,0 +1,24 @@
+#ifndef PWM_H_
+#define PWM_H_
+
+#include "msp.h"
+
+/*  MATH FOR TICKS: Where divider = 1
+ * (3*10^6)/(2^0) = 3000000 |||| 300000*(500*10^-6) = 15 ticks ==> 0xF in hex
+ * After running analysis with TICKS = 15, we noticed we were getting 94Khz which meant our pwm was running slightly slow. To counteract
+ * this, we set TICKS = 14 = 0xE. We are guess this was due to the fact an entire tick period is required to reset the counter to 0.
+ */
+#define TICKS (0xE)
+
+void config_pwm_timer(void);
+
+void start_pwm(void);
+
+void stop_pwm(void);
+
+void config_pwm_gpio(void);
+
+void config_switch_interrupt(void);
+
+
+#endif /* PWM_H_ */
